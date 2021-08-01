a560a89c9bc91981056aaf5c1b299c10367a8e11
joshwrn <joshnwarren@gmail.com>
2021-08-01 02:54:28 -0700

    switched to hash router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import '../styles/app.css';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Nav from './Nav/Nav';
 import Home from './Home/Home';
 import Profile from './Profile/Profile';
@@ -11,42 +11,36 @@ import Messages from './Messages/Messages';
 import { AuthProvider } from '../contexts/AuthContext';
 
 function App() {
-  const BrowserRouter = require('react-router-dom').BrowserRouter;
-  // memory leak from auth provider context even when everything is removed
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Router>
-        <AuthProvider>
-          <div className="App">
-            {/*//+ Nav */}
-            <Nav />
-            <Switch>
-              {/*//+ Home */}
-              <Route exact path="/instagram-clone">
-                <Home />
-              </Route>
-              {/*//+ Messages */}
-              <Route exact path="/messages/:uid" component={Messages} />
-              <Route exact path="/messages">
-                <Messages />
-              </Route>
-              {/*//+ Sign up */}
-              <Route exact path="/sign-up">
-                <SignUp />
-              </Route>
-              {/*//+ Profile */}
-              <Route exact path="/profile/:uid" component={Profile} />
-              {/*//+ post */}
-              <Route exact path="/post/:uid/:postid" component={Post} />
-              {/*//+ settings */}
-              <Route exact path="/settings">
-                <Settings />
-              </Route>
-            </Switch>
-          </div>
-        </AuthProvider>
-      </Router>
-    </BrowserRouter>
+    <HashRouter>
+      <AuthProvider>
+        <div className="App">
+          {/*//+ Nav */}
+          <Nav />
+          {/*//+ Home */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+          {/*//+ Messages */}
+          <Route exact path="/messages/:uid" component={Messages} />
+          <Route exact path="/messages">
+            <Messages />
+          </Route>
+          {/*//+ Sign up */}
+          <Route exact path="/sign-up">
+            <SignUp />
+          </Route>
+          {/*//+ Profile */}
+          <Route exact path="/profile/:uid" component={Profile} />
+          {/*//+ post */}
+          <Route exact path="/post/:uid/:postid" component={Post} />
+          {/*//+ settings */}
+          <Route exact path="/settings">
+            <Settings />
+          </Route>
+        </div>
+      </AuthProvider>
+    </HashRouter>
   );
 }
 
